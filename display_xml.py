f25bd5f5ec7418f52238f7547b6e1863009a78af
panuscha <charlottepanuskova@gmail.com>
2023-06-26 16:41:12 +0200

    update display xml

--- display_xml.py
+++ display_xml.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-from pymarc import map_xml
-
-file_path = 'data/ucla.xml'
-
-database = ['B12', 'B45', 'B97', 'B70', 'INT', 'MBK-PL-OPO',  ]
-
-def print_record(r):
-    for field in r.get_fields('964'):
-        if field['a'] not in database:
-            print(field['a'])
-            database.append(field['a'])
-
-=======
 from pymarc import map_xml
 
 file_path = 'data/ucla.xml'
@@ -19,5 +5,4 @@ file_path = 'data/ucla.xml'
 def print_record(r):
     print(r)
 
->>>>>>> origin/main
 map_xml(print_record, file_path)
